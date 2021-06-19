ba74788833c3f6bdf2a97b57fd3e871e43ebe24f
Evgeniy <ferno1@yandex.ru>
2021-06-19 14:39:40 +0300

    tag upd

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm, ModelMultipleChoiceField
 
 from .models import Ingredient, Recipe, Tag
@@ -8,7 +9,10 @@ class RecipeForm(ModelForm):
         model = Recipe
         fields = ('title', 'time', 'text', 'image', 'tags', 'ingredients')
 
-        tags = ModelMultipleChoiceField(queryset=Tag.objects.all())
+        tags = ModelMultipleChoiceField(
+            queryset=Tag.objects.all(),
+            widget=forms.CheckboxSelectMultiple
+        )
         ingredients = ModelMultipleChoiceField(
             queryset=Ingredient.objects.all()
         )
