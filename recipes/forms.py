184d9135f6b28bf4685d7be3125b600ee8a3ce0d
Evgeniy <ferno1@yandex.ru>
2021-06-19 23:27:37 +0300

    form func to forms

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
+from django.shortcuts import get_object_or_404
 
-from .models import Recipe
-from .views import form_ingredients_tags
+from .models import Ingredient, Recipe, Tag
 
 
 class RecipeForm(ModelForm):
@@ -17,3 +17,39 @@ class RecipeForm(ModelForm):
         if not form_tags:
             self.add_error(None, 'Выберите тег')
         return self.cleaned_data
+
+
+def form_ingredients_tags(request):
+    form_ingredients = {}
+    form_tags = []
+    ing_part = []
+    tag_keys = Tag.objects.values_list('slug', flat=True)
+    all_ingredients = Ingredient.objects.values_list('title', flat=True)
+    ing_keys = ['nameIngred', 'valueIngre', 'unitsIngre']
+    for field in request:
+        if field in tag_keys:
+            form_tags.append(get_object_or_404(Tag, slug=field).id)
+            continue
+        if field[:10] not in ing_keys:
+            continue
+        ing_part.append(request[field])
+        if len(ing_part) != 3:
+            continue
+        title = ing_part[0]
+        if title not in all_ingredients:
+            ing_part = []
+            continue
+        amount = float(ing_part[1].replace(',', '.'))
+        dimension = ing_part[2]
+        if title in form_ingredients:
+            form_ingredients[title][1] += amount
+        else:
+            ing = get_object_or_404(
+                Ingredient,
+                title=title,
+                dimension=dimension
+            )
+            form_ingredients[title] = [ing, amount]
+        ing_part = []
+
+    return list(form_ingredients.values()), form_tags
--- recipes/views.py
+++ recipes/views.py
@@ -9,10 +9,9 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 from foodgram.settings import PAGE_SIZE_CART, PAGE_SIZE_INDEX
 from recipes.models import (
-    Cart, Ingredient, Recipe, RecipeIngredient, Subscription, Tag, User
+    Cart, Recipe, RecipeIngredient, Subscription, Tag, User
 )
-
-from .forms import RecipeForm
+from .forms import RecipeForm, form_ingredients_tags
 
 
 class RecipeListView(ListView):
@@ -121,42 +120,6 @@ class CartListView(LoginRequiredMixin, RecipeListView):
         qs = super().get_queryset()
         qs = qs.filter(customer=self.request.user)
         return qs
-
-
-def form_ingredients_tags(request):
-    form_ingredients = {}
-    form_tags = []
-    ing_part = []
-    tag_keys = Tag.objects.values_list('slug', flat=True)
-    all_ingredients = Ingredient.objects.values_list('title', flat=True)
-    ing_keys = ['nameIngred', 'valueIngre', 'unitsIngre']
-    for field in request:
-        if field in tag_keys:
-            form_tags.append(get_object_or_404(Tag, slug=field).id)
-            continue
-        if field[:10] not in ing_keys:
-            continue
-        ing_part.append(request[field])
-        if len(ing_part) != 3:
-            continue
-        title = ing_part[0]
-        if title not in all_ingredients:
-            ing_part = []
-            continue
-        amount = float(ing_part[1].replace(',', '.'))
-        dimension = ing_part[2]
-        if title in form_ingredients:
-            form_ingredients[title][1] += amount
-        else:
-            ing = get_object_or_404(
-                Ingredient,
-                title=title,
-                dimension=dimension
-            )
-            form_ingredients[title] = [ing, amount]
-        ing_part = []
-
-    return list(form_ingredients.values()), form_tags
 
 
 def recipe_ingredient_bulk_create(form_ingredients, recipe):
